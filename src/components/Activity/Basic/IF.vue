868de43efc9d128cee4aad1217bb4ea5665504d8
Ömer Faruk Şahin <ofaruksahin@outlook.com.tr>
2021-02-28 20:57:39 +0300

    #7

---
+++ src/components/Activity/Basic/IF.vue
@@ -0,0 +1,37 @@
+<template>
+  <v-dialog
+    v-model="dialog"
+    persistent
+    max-width="600px"
+    v-if="Object.keys(activity).length > 0"
+  >
+    <v-card>
+      <h1 class="title">IF</h1>
+    </v-card>
+  </v-dialog>
+</template>
+
+<script>
+export default {
+    props:{
+        activity:{
+            required:true,
+            default:{}
+        },
+        variables:{
+            required:true,
+            default:[]
+        }
+    },
+    data(){
+        return{
+            dialog : false
+        }
+    },
+    methods:{
+        openDialog(){
+            
+        }
+    }
+};
+</script>
