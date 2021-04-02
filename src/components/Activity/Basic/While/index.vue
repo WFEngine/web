d0dc05f58807daeca0ecfac7d1471487e35a7b39
Ömer Faruk Şahin <ofaruksahin@outlook.com.tr>
2021-04-02 22:18:50 +0300

    #7

--- src/components/Activity/Basic/While/index.vue
+++ src/components/Activity/Basic/While/index.vue
@@ -14,7 +14,58 @@
         <v-toolbar-title>{{ activity.ActivityName }}</v-toolbar-title>
       </v-toolbar>
       <v-card-actions>
-        <v-row class="ma-0"></v-row>
+        <v-row class="ma-0">
+          <v-col
+            cols="12"
+            offset-md="1"
+            offset-lg="1"
+            offset-xl="1"
+            md="10"
+            lg="10"
+            xl="10"
+            class="pa-0"
+          >
+            <v-card>
+              <v-toolbar class="gradient" dark>
+                <h1 class="title">While</h1>
+                <v-spacer></v-spacer>
+                <v-btn icon>
+                  <v-icon>fa fa-plus</v-icon>
+                </v-btn>
+              </v-toolbar>
+              <v-card-actions>
+                <v-row class="ma-0">
+                  <v-col cols="12" md="4" lg="4" xl="4">
+                    <v-text-field
+                      v-model="activity.Arguments[0].Value[0].LeftItem.Value[0]"
+                      outlined
+                      label="Left Item"
+                    ></v-text-field>
+                  </v-col>
+                  <v-col cols="12" md="4" lg="4" xl="4">
+                    <v-autocomplete
+                      v-model="activity.Arguments[0].Value[0].Operator"
+                      :items="conditions"
+                      item-text="Label"
+                      item-value="Label"
+                      outlined
+                      label="Operator"
+                    ></v-autocomplete>
+                  </v-col>
+                  <v-col cols="12" md="4" lg="4" xl="4">
+                    <v-text-field
+                      v-model="
+                        activity.Arguments[0].Value[0].RightItem.Value[0]
+                      "
+                      outlined
+                      label="Right Item"
+                    ></v-text-field>
+                  </v-col>
+                </v-row>
+              </v-card-actions>
+            </v-card>
+          </v-col>
+        </v-row>
       </v-card-actions>
     </v-card>
   </v-dialog>
@@ -24,7 +75,7 @@
 import getVariableType from "../../../../entities/variabletype/get";
 import { GET_VARIABLE_TYPES } from "../../../../store/modules/variabletype/actions.type";
 import { ShowErrorMessage } from "../../../../common/alerts";
-
+import conditions from "../Condition/ConditionType";
 export default {
   props: {
     activity: {
@@ -40,6 +91,7 @@ export default {
       tab: "",
       variableTypes: [],
       activities: [],
+      conditions: conditions,
     };
   },
   methods: {
@@ -64,8 +116,8 @@ export default {
   },
   created() {
     if (this.variableTypes.length == 0) {
-        this.getVariableTypes();
-        this.activities = this.$store.getters.getActivities;
+      this.getVariableTypes();
+      this.activities = this.$store.getters.getActivities;
     }
   },
   watch: {
@@ -101,6 +153,8 @@ export default {
               ],
             });
           }
+
+          console.log(this.activity);
         }
       },
     },
