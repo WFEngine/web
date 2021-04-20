6893a830562500ad0e933109471e5e0c8b035a93
Ömer Faruk Şahin <ofaruksahin@outlook.com.tr>
2021-04-20 21:49:18 +0300

    #7

--- src/components/Activity/Basic/For/index.vue
+++ src/components/Activity/Basic/For/index.vue
@@ -6,6 +6,15 @@
     transition="dialog-bottom-transition"
     v-if="dialog"
   >
+    <wf-designer-toolbar
+      :activities="activities"
+      :show="toolbarDialog"
+      :selectedActivity="selectedActivity"
+      v-on:dialogClosed="toolbarClosed"
+      v-on:activityDbClick="activityAdded"
+      v-on:selectedActivityRemove="selectedActivityRemoved"
+      ref="toolbar"
+    ></wf-designer-toolbar>
     <v-card>
       <v-toolbar dark class="gradient" tile>
         <v-btn icon dark @click="closeDialog">
@@ -43,27 +52,71 @@
                     offset-md="1"
                     offset-lg="1"
                     offset-xl="1"
+                    class="pa-0"
                   >
                     <v-text-field
                       v-model="activity.Arguments[0].Value[0]"
                       label="Start Value"
                       outlined
+                      @change="startValueChanged"
                     ></v-text-field>
                   </v-col>
-                  <v-col
-                    cols="12"
-                    md="4"
-                    lg="4"
-                    xl="4"
-                    offset-md="1"
-                    offset-lg="1"
-                    offset-xl="1"
-                  >
+                  <v-col cols="12" md="6" lg="6" xl="6" class="pa-0">
                     <v-autocomplete
                       v-model="activity.Arguments[0].ArgumentType"
                       label="Start Value Type"
+                      :items="variableTypes"
+                      item-text="type"
+                      item-value="type"
                       outlined
                     ></v-autocomplete>
+                  </v-col>
+                  <v-col cols="12" md="6" lg="6" xl="6" class="pa-0">
+                    <v-text-field
+                      v-model="activity.Arguments[1].Value[0].LeftItem.Value[0]"
+                      label="Left Item"
+                      @change="leftItemChanged"
+                      outlined
+                    ></v-text-field>
+                  </v-col>
+                  <v-col cols="12" md="6" lg="6" xl="6" class="pa-0">
+                    <v-autocomplete
+                      v-model="activity.Arguments[1].Value[0].Operator"
+                      :items="conditionTypes"
+                      item-text="Label"
+                      item-value="Label"
+                      outlined
+                      label="Operator"
+                    ></v-autocomplete>
+                  </v-col>
+                  <v-col cols="12" md="6" lg="6" xl="6" class="pa-0">
+                    <v-text-field
+                      v-model="
+                        activity.Arguments[1].Value[0].RightItem.Value[0]
+                      "
+                      label="Left Item"
+                      @change="rightItemChanged"
+                      outlined
+                    ></v-text-field>
+                  </v-col>
+                  <v-col cols="12" md="6" lg="6" xl="6" class="pa-0">
+                    <v-text-field
+                      v-model="activity.Arguments[2].Value[0]"
+                      label="Counter"
+                      outlined
+                    ></v-text-field>
+                  </v-col>
+                  <v-col cols="12" class="pa-0">
+                    <v-card>
+                      <v-expansion-panels multiple>
+                        <activity-item
+                          v-if="activity.Blocks != undefined && dialog"
+                          :activity="activity"
+                          :variables="variables"
+                          v-on:variableButtonClick="variableButtonClicked"
+                        ></activity-item>
+                      </v-expansion-panels>
+                    </v-card>
                   </v-col>
                 </v-row>
               </v-card-actions>
@@ -76,6 +129,11 @@
 </template>
 
 <script>
+import getVariableType from "../../../../entities/variabletype/get";
+import { GET_VARIABLE_TYPES } from "../../../../store/modules/variabletype/actions.type";
+import { ShowErrorMessage } from "../../../../common/alerts";
+import conditionTypes from "../Condition/ConditionType";
+import generateGuid from "../../../../common/guid";
 export default {
   props: {
     activity: {
@@ -89,6 +147,10 @@ export default {
     return {
       dialog: false,
       toolbarDialog: false,
+      variableTypes: [],
+      conditionTypes: conditionTypes,
+      selectedActivity: {},
+      activities: [],
     };
   },
   methods: {
@@ -98,6 +160,92 @@ export default {
     closeDialog() {
       this.dialog = false;
     },
+    startValueChanged() {
+      var variableName = this.activity.Arguments[0].Value[0];
+      if (variableName.startsWith("$"))
+        variableName = variableName.substring(1);
+      this.variables
+        .filter((x) => x.Name == variableName)
+        .map((m) => {
+          this.activity.Arguments[0].IsVariable = true;
+          this.activity.Arguments[0].ArgumentType = m.Type;
+          this.activity.Arguments[2].ArgumentType = m.Type;
+          return;
+        });
+    },
+    getVariableTypes() {
+      var req = Object.assign({}, getVariableType);
+      req.ProjectId = parseInt(this.$route.params.projectid);
+      this.$store
+        .dispatch(GET_VARIABLE_TYPES, req)
+        .then(() => {
+          this.variableTypes = this.$store.getters.getVariableTypes;
+        })
+        .catch((err) => {
+          ShowErrorMessage(err.message);
+        });
+    },
+    leftItemChanged() {
+      var variableName = this.activity.Arguments[1].Value[0].LeftItem.Value[0];
+      if (variableName.startsWith("$"))
+        variableName = variableName.substring(1);
+      this.variables
+        .filter((x) => x.Name == variableName)
+        .map((m) => {
+          this.activity.Arguments[1].Value[0].LeftItem.IsVariable = true;
+          this.activity.Arguments[1].Value[0].LeftItem.ArgumentType = m.Type;
+          return;
+        });
+    },
+    rightItemChanged() {
+      var variableName = this.activity.Arguments[1].Value[0].RightItem.Value[0];
+      if (variableName.startsWith("$"))
+        variableName = variableName.substring(1);
+      this.variables
+        .filter((x) => x.Name == variableName)
+        .map((m) => {
+          this.activity.Arguments[1].Value[0].RightItem.IsVariable = true;
+          this.activity.Arguments[1].Value[0].RightItem.ArgumentType = m.Type;
+          return;
+        });
+    },
+    activityAdded(activity) {
+      this.activity.Blocks.push({
+        UniqueKey: generateGuid(),
+        Name: activity.activityName,
+        IsContainer: this.activityIsContainer(activity),
+        AssemblyName: activity.assemblyName,
+        ActivityName: activity.activityName,
+        Variables: [],
+        Arguments: [],
+        Blocks: [],
+      });
+    },
+    activityIsContainer(activity) {
+      switch (activity.activityName) {
+        case "WFEngine.Activities.Basic.Container":
+          return true;
+        default:
+          return false;
+      }
+    },
+    selectedActivityRemoved() {
+      this.selectedActivity = {};
+    },
+    variableButtonClicked(activity) {
+      this.selectedActivity = activity;
+      this.toolbarDialog = true;
+      this.$refs.toolbar.setActiveTab("variable-tab");
+    },
+    toolbarClosed() {
+      this.toolbarDialog = false;
+    },
+  },
+  created() {
+    if (this.variableTypes.length == 0) {
+      this.getVariableTypes();
+      this.activities = this.$store.getters.getActivities;
+    }
   },
   watch: {
     activity: {
@@ -106,7 +254,7 @@ export default {
           if (this.activity.Arguments.length < 1) {
             this.activity.Arguments.push({
               Name: "StartValue",
-              ArgumentType: "System.Int32",
+              ArgumentType: "",
               IsVariable: false,
               IsConstant: false,
               Value: [""],
